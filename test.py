a3846900b8ce9c13661001c5cd409145a427519c
Xuejun Zhu <xuejun.zhu@gmail.com>
2021-01-31 00:37:20 -0500

    Add a test.py on 20210131

---
+++ test.py
@@ -0,0 +1,30 @@
+
+#%%
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+os.getcwd()
+os.chdir("C:/Temp")
+
+my_x = np.linspace(-1,1) 
+my_y = np.sin(my_x) 
+
+plt.plot(my_x,my_y) 
+title = "Plot"
+filename = "plot.jpg" 
+
+plt.title(title) 
+plt.savefig(filename)  
+plt.show()
+
+
+
+
+
+
+
+
+
+
+# %%
